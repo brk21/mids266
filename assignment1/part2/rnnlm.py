e4bb871df4ffcec0d3e9bca9f0a5a6fed05dcb60
brk21 <rkatz@2u.com>
2016-11-06 19:49:30 -0600

    rnnlm final changes

--- assignment1/part2/rnnlm.py
+++ assignment1/part2/rnnlm.py
@@ -21,15 +21,12 @@ def matmul3d(X, W):
 
 def MakeFancyRNNCell(H, keep_prob, num_layers=1):
   """Make a fancy RNN cell.
-
   Use tf.nn.rnn_cell functions to construct an LSTM cell.
   Initialize forget_bias=0.0 for better training.
-
   Args:
     H: hidden state size
     keep_prob: dropout keep prob (same for input and output)
     num_layers: number of cell layers
-
   Returns:
     (tf.nn.rnn_cell.RNNCell) multi-layer LSTM cell with dropout
   """
@@ -44,16 +41,6 @@ def MakeFancyRNNCell(H, keep_prob, num_layers=1):
 
   #### END(YOUR CODE) ####
   return cell
-
-def unpack_sequence(tensor):
-    """Split the single tensor of a sequence into a list of frames. 
-    FROM: https://danijar.com/introduction-to-recurrent-networks-in-tensorflow/"""
-    return tf.unpack(tf.transpose(tensor, perm=[1, 0, 2]))
-
-def pack_sequence(sequence):
-    """Combine a list of the frames into a single tensor of the sequence.
-    FROM: https://danijar.com/introduction-to-recurrent-networks-in-tensorflow/"""
-    return tf.transpose(tf.pack(sequence), perm=[1, 0, 2])
 
 class RNNLM(object):
 
@@ -139,67 +126,35 @@ class RNNLM(object):
     self.ns_ = tf.tile([self.max_time_], [self.batch_size_,], name="ns")
 
     #### YOUR CODE HERE ####
-    with tf.name_scope("inputs"):
-        ### HOW DO I ACCESS THE INTEGER VERSION OF THE BATCH SIZE AND MAX TIME TO PUT THEM INO THE SHAPES BELOW?
-        ### EVERYTIME I TRY TO ACCESS WITH self.batch_size_ or self.ns_[1] or self.batch_size_.get_shape()[0] 
-        ### or tf.shape(self.ns_[1]), I GET ERRORS THAT THESE ARE TENSORS OR SHAPES AND NOT INTEGERS
-        #[batch_size, max_time]
-        self.input_w_ = tf.placeholder(tf.int32, [None, None], name="w")
-        #[batch_size, max_time]
-        self.target_y_ = tf.placeholder(tf.int32, [None, None], name="y")
-        #[batch_size]
-        self.initial_h_ = tf.placeholder(tf.int32,[self.H],name="h_init")
-    
     # Construct embedding layer
     with tf.name_scope("embedding_layer"):
-    ### EMBEDDING MATRIX IS V X H, H EMBEDDINGS FOR EVERY WORD ID IN THE VOCABULARY. IS THAT CORRECT? 
-    ### CONFUSED BY THE INSTRUCTION ABOVE THAT H REPRESENTS THE HIDDEN STATE. 
-    ### DOES THAT ALSO INCLUDE THE BREADTH OF THE EMBEDDING MATRIX?
-        C_ = tf.get_variable(name="C", shape=[self.V,self.H],
+        self.C_ = tf.get_variable(name="C", shape=[self.V,self.H],
                             dtype=tf.float32,initializer=tf.random_uniform_initializer(minval=-1, maxval=1))
-    ### embedding_lookup gives shape (batch_size, N, M) FROM WEEK 4 IPYNB
-    ### EMBEDDING LOOKUP LOOKS UP EACH ID TO GET THE ROW OUT OF THE EMBEDDING MATRIX
-    ### I STOLE THIS CODE FROM THE WEEK 4 IPYNB, BUT THE SHAPE LOOKS WRONG TO ME. SHOULD THE NUMBER OF LOOKUPS BE EQUAL TO THE BATCH SIZE
-        x_ = tf.reshape(tf.nn.embedding_lookup(C_, self.input_w_), 
-                    [-1, self.V*self.H], name="x")
-        
-    ### DO I NEED A BIAS TERM IN THE EMBEDDING LAYER? ASSUMING AS OF NOW THAT THE ANSWER IS "NO"
-        #b = tf.get_variable(name="b_embedding", shape=[self.V],
-        #                   dtype=tf.float32,initializer=tf.constant_initializer(value=0.0, dtype=tf.float32))
-        #b1 = tf.nn.embedding_lookup(b, self.input_w_)
+        self.x_ = tf.nn.embedding_lookup(self.C_, self.input_w_)
 
     # Construct RNN/LSTM cell and recurrent layer
     with tf.name_scope("LSTM_RNN_layer_s"):
         # CREATE THE FANCY RNN
         cell = MakeFancyRNNCell(self.H, self.dropout_keep_prob_, self.num_layers)
         # SET H_INIT EQUAL TO THE CELL'S ZERO STATE AS EXPLAINED ABOVE
-        self.initial_h_ = cell.zero_state(self.ns_[1], dtype=tf.float32)
+        self.initial_h_ = cell.zero_state(self.batch_size_, dtype=tf.float32)
         # GET THE OUTPUT AND NEW CELL STATE OUT OF THE DYNAMIC RNN THAT COMBINES 
         # THE NUMBER OF CELLS BASED ON THE H (HIDDEN LAYER DIMENSION) PARAMETER
-        ### GETTING THE FOLLOWING ERROR WITH THE BELOW: 
-        outputs, states = tf.nn.dynamic_rnn(cell, unpack_sequence(self.initial_h_), dtype=tf.float32)
-        # USING THE PACK AND UNPACK FUNCTIONS PER https://danijar.com/introduction-to-recurrent-networks-in-tensorflow/
-        self.output_ = pack_sequence(outputs)
-        self.cell_state_ = pack_sequence(states)
-        # SETTING THE INITIAL H BACK EQUAL TO THE OUTPUT, BUT NOT CERTAIN THIS IS NECESSARY. 
-        # HOW DOES THIS H_OUTPUT PERSIST INTO FUTURE ITERATIONS OF THE NETWORK?
-        # DO WE NEED H_INIT ANYMORE?
-        ### WHAT'S THE DIFFERENCE BETWEEN THE INIITAL_H AND THE STATE OF THE CELL?
-        ### HOW ARE THE H_INIT, CELL STATE, AND H_OUTPUT COMBINED INTO THE FOLLOWING?
-        ### 1) CONCATENATION OF H WITH X BELOW (I.E. ARE WE CONCATENATING THE CELL STATE, H_INIT, OR THE OUTPUT WITH THE X_ EMBEDDINGS ABOVE?
-        ### 2) ARE THE LOGITS THE RESULT OF THE (OUTPUT * CONCATENATED STATES + BIASES) AS INDICATED BELOW? 
+        self.output_, self.cell_state_ = tf.nn.dynamic_rnn(cell, 
+                                            inputs=self.x_,
+                                            initial_state=self.initial_h_,
+                                            dtype=tf.float32)
+        self.final_h_ = self.cell_state_
 
     # Softmax output layer, over vocabulary
     # Hint: use the matmul3d() helper here.
-    ### IS THE OUTPUT OF THE DYNAMIC RNN ABOVE THE SAME AS W23 BELOW FROM THE WEEK 4 IPYNB? THAT'S HOW I HAVE IT RIGHT NOW
     with tf.name_scope("output_layer"):
-        # Concat [h x] and [W2 W3]
-        self.b3_ = tf.get_variable(name="b_output", shape=[self.ns_[1]],
+        self.w_out = tf.get_variable(name="w_out", shape=[self.H, self.V],
                            dtype=tf.float32,initializer=tf.constant_initializer(value=0.0, dtype=tf.float32))
-        self.hx_ = tf.concat(1, [self.cell_state_, x_], name="hx")
-        #W23_ = tf.concat(0, [W2_, W3_], name="W23")
-        self.logits_ = tf.add(tf.matmul3d(self.output_,self.hx_), self.b3_, name="logits")
-        ### THE SOFTMAX HAPPENS IN THE LOSS COMPUTATION BELOW, RIGHT? THIS IS JUST THE MATRIX MULTIPLICATION. WHERE IS THE CONCATENATION? DOES ALL OF THAT HAPPEN IN THE APIS?
+        self.b3_ = tf.get_variable(name="b_output", shape=[self.V],
+                           dtype=tf.float32,initializer=tf.constant_initializer(value=0.0, dtype=tf.float32))
+        self.logits_ = tf.add(matmul3d(self.output_,self.w_out), self.b3_, name="logits")
+        
 
     # Loss computation (true loss, for prediction)
     with tf.name_scope("full_loss_computation"):
@@ -230,14 +185,17 @@ class RNNLM(object):
     #### YOUR CODE HERE ####
     # Define loss function(s)
     with tf.name_scope("train_loss_computation"):
-      # Placeholder: replace with a sampled loss
-      self.train_loss_ = tf.reduce_sum(tf.nn.sampled_softmax_loss(tf.transpose(self.output_),
-                                                                 self.b3_, self.hx_, 
-                                             labels=tf.expand_dims(self.target_y_, 1), 
-                                             num_sampled=100, num_classes=self.V,
-                                             name="per_example_sampled_softmax_loss"),
+        self.train_loss_ = tf.reduce_sum(tf.nn.sampled_softmax_loss(weights = tf.transpose(self.w_out),
+                                                                  biases = self.b3_, 
+                                                                  inputs = tf.reshape(self.output_,
+                                                                                 [self.batch_size_ * self.max_time_, self.H]), 
+                                                                  labels = tf.reshape(self.target_y_,
+                                                                                 [self.batch_size_ * self.max_time_, 1]), 
+                                                                  num_sampled = 100, 
+                                                                  num_classes=self.V,
+                                                                  num_true = 1,
+                                                                  name="per_example_sampled_softmax_loss"),
                                     name="sampled_softmax_loss")
-
 
     # Define optimizer and training op
     with tf.name_scope("training"):
@@ -259,8 +217,8 @@ class RNNLM(object):
 
     #### YOUR CODE HERE ####
     with tf.name_scope("Prediction"):
-        self.pred_samples_ = tf.reshape(tf.multinomial(tf.reshape(self.logits_,[-1,self.batch_size_]),
-                                                       self.batch_size_,name ="pred_random"),[-1,self.max_time_,1])
-        ### WHERE ARE THE MULTIPLE CALLS TO TF.RESHAPE? WHY CAN'T YOU JUST RESHAPE ONCE?
+        self.pred_samples_ = tf.multinomial(tf.reshape(self.logits_,[-1,self.V]),
+                                                       num_samples=1,name ="pred_random")
+        ###  HERE ARE THE MULTIPLE CALLS TO TF.RESHAPE? WHY CAN'T YOU JUST RESHAPE ONCE?
     #### END(YOUR CODE) ####
 
